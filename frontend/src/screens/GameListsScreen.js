14d811148d9aede17b3c3ee7f921fbfc13b4e8b8
Shinichiro Suzuki <shintrfc@gmail.com>
2022-03-08 13:10:55 +0900

    gamelist screen fetch data

--- frontend/src/screens/GameListsScreen.js
+++ frontend/src/screens/GameListsScreen.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import Container from '../components/common/Container'
 import ItemContainer from '../components/common/ItemContainer'
+import Loader from '../components/common/Loader'
 import Lists from '../components/Lists'
-import posts from '../data/posts'
+import { listGames } from '../redux/actions/gameActions'
 
 const GameListsScreen = () => {
+	const dispatch = useDispatch()
+
+	const gameList = useSelector((state) => state.gameList)
+	const { games, loading } = gameList
+
+	useEffect(() => {
+		dispatch(listGames())
+	}, [dispatch])
 	return (
 		<Container>
 			<h1>Game Lists</h1>
-			<Lists>
-				{posts.map((post) => (
-					<ItemContainer
-						key={post._id}
-						item={post.title}
-						gameName={post.gameName}
-					/>
-				))}
-			</Lists>
+			{loading ? (
+				<Loader />
+			) : (
+				<Lists>
+					{games &&
+						games.map((game) => (
+							<Link to={`/games/${game._id}`} key={game._id}>
+								<ItemContainer
+
+								// item={game.title}
+								// gameName={game.gameName}
+								>
+									{game.title}
+								</ItemContainer>
+							</Link>
+						))}
+				</Lists>
+			)}
 		</Container>
 	)
 }
