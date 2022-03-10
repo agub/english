896ebb2aef40970f804d20796e7b4b33e0acbc70
Shinichiro Suzuki <shintrfc@gmail.com>
2022-03-10 19:32:26 +0900

    verifyUserScreen fixed

--- frontend/src/screens/VerifyUserScreen.js
+++ frontend/src/screens/VerifyUserScreen.js
@@ -2,28 +2,43 @@ import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { verifyUser } from '../redux/actions/userActions'
+import Loader from '../components/common/Loader'
+import Container from '../components/common/Container'
+import FormContainer from '../components/common/FormContainer'
 
-const VerifyUserScreen = ({ match, history }) => {
+const VerifyUserScreen = () => {
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 	const { id, token } = useParams()
 
-	// const cart = useSelector((state) => state.cart)
-	// const { cartItems } = cart
-
-	// const userVerify = useSelector((state) => state.userVerify)
-	// const { success } = userVerify
-
-	const success = false
+	const userVerify = useSelector((state) => state.userVerify)
+	const { success, error } = userVerify
 
 	useEffect(() => {
 		if (!success) {
 			dispatch(verifyUser(id, token))
 			console.log(id, token)
-			navigate('/login')
 		}
+		if (success) navigate('/login')
 	}, [success, dispatch, navigate, id, token]) // eslint-disable-line react-hooks/exhaustive-deps
-	return null
+	return (
+		<>
+			<Container>
+				<FormContainer>
+					{!success || !error ? (
+						<>
+							<h1 className='text-center'>
+								メールアドレスの確認中です...
+							</h1>
+							<Loader />
+						</>
+					) : (
+						<div>エラー: {error}</div>
+					)}
+				</FormContainer>
+			</Container>
+		</>
+	)
 }
 
 export default VerifyUserScreen
